cc5c357e887fd5d4da2c8fb467939685c5472ecb
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-01-12 14:50:50 +0900

    [level Lv.2] Title: 롤케이크 자르기, Time: 612.11 ms, Memory: 51.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -0,0 +1,18 @@
+from collections import Counter
+
+def solution(topping):
+    answer = 0
+    cs = Counter(topping)
+    br = set()
+    
+    for t in topping:
+        cs[t] -= 1
+        br.add(t)
+        
+        if cs[t]==0:
+            cs.pop(t)
+            
+        if len(cs) == len(br):
+            answer += 1
+            
+    return answer
