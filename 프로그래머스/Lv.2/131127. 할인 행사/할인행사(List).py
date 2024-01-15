251ac331c28a446914d37ab79d01b8d85d4e87dc
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-01-15 18:56:35 +0900

    Create 할인행사(List).py

---
+++ 프로그래머스/Lv.2/131127. 할인 행사/할인행사(List).py
@@ -0,0 +1,16 @@
+def solution(want, number, discount):
+    answer = 0
+    list_all_want = []
+
+    for i in range(len(want)):
+        for j in range(number[i]):
+            list_all_want.append(want[i])
+    list_all_want.sort()
+
+    for i in range(len(discount) - 9):
+        list_10 = discount[i: i+10]
+        list_10.sort()
+        if list_all_want == list_10:
+            answer += 1
+
+    return answer
