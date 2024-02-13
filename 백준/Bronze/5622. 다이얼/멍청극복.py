db2abad76bc464ff8f1ce839bdb480171fcfdb83
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-02-13 16:45:42 +0900

    Update 멍청극복.py

--- 백준/Bronze/5622. 다이얼/멍청극복.py
+++ 백준/Bronze/5622. 다이얼/멍청극복.py
@@ -1,11 +1,8 @@
 dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
-
-alpha = list(input())		// 알파벳 단어 입력
-result = 0
-
-for i in alpha:
-    for j in dial:
-        if i in str(j):		// str(j) = 'A', 'B', 'C', •••
-            num = dial.index(j) + 3		// 각 알파벳 별 필요한 시간
-            result += num
-print(result)
+a = input()
+ret = 0
+for j in range(len(a)):
+    for i in dial:
+        if a[j] in i:
+            ret += dial.index(i)+3
+print(ret)
