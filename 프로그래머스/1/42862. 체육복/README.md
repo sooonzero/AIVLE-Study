96087c2fb87753217b0bd272d496f0813a7f6f5e
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-03-05 19:32:42 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862#qna) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 05일 19:29:25
+2024년 03월 05일 19:32:41
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,41 +1,18 @@
-# 전체에서 체육복 못 빌리는 학생 수 만큼 빼기
 def solution(n, lost, reserve):
     lost_set = set(lost)
     reserve_set = set(reserve)
-    
-    reserve_and_lost = lost_set & reserve_set
+    answer = n
 
+    #여분이 있는데 도난당한 경우
+    reserve_and_lost = lost_set & reserve_set
     lost_set -= reserve_and_lost
     reserve_set -= reserve_and_lost
-    
-    answer = n
-    
+
     for x in reserve_set:
         if x-1 in lost_set:
             lost_set.remove(x-1)
-            
         elif x+1 in lost_set:
             lost_set.remove(x+1)
-            
-            
+
     answer -= len(lost_set)
-    
-    
     return answer
-'''
-def solution(n, lost, reserve):
-    lost_set = set(lost)
-    reserve_set = set(reserve)
-    reserve_and_lost = lost_set & reserve_set
-
-    lost_set -= reserve_and_lost
-    reserve_set -= reserve_and_lost
-
-    for i in reserve_set:
-        if i - 1 in lost_set:
-            lost_set -= {i - 1, }
-        elif i + 1 in lost_set:
-            lost_set -= {i + 1, }
-
-    return n - len(lost_set) 
-'''
