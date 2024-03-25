37c54d00d243971d250dc759a556f0c9de76dc5a
이재영 <82437385+sooonzero@users.noreply.github.com>
2024-03-25 23:01:00 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,20 @@
+def solution(a, b, c):
+    answer = 0
+    count = []
+    count.append(a)
+    count.append(b)
+    count.append(c)
+    k = len(set(count))
+    
+    x = a+b+c
+    y = a**2 + b**2 + c**2
+    z = a**3 + b**3 + c**3
+    
+    if k == 3:
+        answer = x
+    elif k == 2:
+        answer = x*y
+    elif k == 1:
+        answer = x*y*z
+    
+    return answer
